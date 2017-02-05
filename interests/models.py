1a79bf3fc24efcf3d097b2aa24e2084f7de21a00
R-Plascencia <runnerdude08@gmail.com>
2017-02-04 19:39:03 -0800

    Adding interests

---
+++ interests/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Interest(models.Model):
+    title = models.CharField(max_length=50)
+    keywords = models.TextField()
+    pub_date = models.DateTimeField()
+    num_of_imports = models.IntegerField(default=1)
+    creator = models.ForeignKey(User)
---
+++ interests/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from .models import Interest
+from django.contrib.auth.models import User
+
+# Create your views here.
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['keywords']:
+            interest = Interest()
+            interest.title = request.POST['title']
+            interest.keywords = request.POST['keywords']
+            interest.pub_date = timezone.datetime.now()
+            interest.creator = request.user
+            interest.save()
+            return redirect('home')
+        else:
+            return render(request, 'interests/create.html', {'error':'ERROR: Interest must have a title and maximum of 4 keywords'})
+    else:
+        return render(request, 'interests/create.html')
+
+def home(request):
+    interests = Interest.objects.order_by('pub_date')
+    return render(request, 'interests/home.html', {'interests':interests})
