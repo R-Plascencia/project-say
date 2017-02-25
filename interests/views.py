d22e50b7659acf68fb7c8e47af192110483add0a
R-Plascencia <runnerdude08@gmail.com>
2017-02-24 23:21:55 -0800

    Retrieving stories working

--- interests/views.py
+++ interests/views.py
@@ -4,6 +4,22 @@ from django.utils import timezone
 from .models import Interest
 from django.contrib.auth.models import User
 from accounts.models import UserProfile
+from django.template.defaulttags import register
+from newsitems.utils import build_results
+import feedparser
+import re
+
+# Dictionary of all allowed RSS sources of news
+# rss_sources = {'AP': 'http://hosted2.ap.org/atom/APDEFAULT/89ae8247abe8493fae24405546e9a1aa',
+#     'Reuters - Politics': 'http://feeds.reuters.com/Reuters/PoliticsNews',
+#     'Reuters - Domestic': 'http://feeds.reuters.com/Reuters/domesticNews',
+#     'The Economist - United States': 'http://www.economist.com/sections/united-states/rss.xml',
+#     'The Economist - Economics': 'http://www.economist.com/sections/economics/rss.xml',
+#     'Financial Times - US': 'http://www.ft.com/rss/world/us',
+#     'Financial Times - US&CAN Politics': 'http://www.ft.com/rss/world/us/politics',
+#     'The Independent': 'http://www.independent.co.uk/news/world/americas/rss',
+#     'BBC': 'http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml'
+# }
 
 # Create your views here.
 @login_required
@@ -17,15 +33,20 @@ def create(request):
             interest.creator = request.user
             interest.save()
             request.user.profile.interests.add(interest)
+            results = build_results(request, interest)
             return redirect('home')
         else:
             return render(request, 'interests/create.html', {'error':'ERROR: Interest must have a title and maximum of 4 keywords'})
     else:
+        print("WHY ARE WE HERE")
         return render(request, 'interests/create.html')
 
 def home(request):
-    interests = Interest.objects.order_by('pub_date')
-    return render(request, 'interests/home.html', {'interests':interests})
+    if request.user.is_anonymous():
+        return render(request, 'interests/home.html')
+    else:
+        interests = request.user.profile.interests
+        return render(request, 'interests/home.html', {'interests': interests})
 
 def list(request):
     interests = Interest.objects.order_by('title')
