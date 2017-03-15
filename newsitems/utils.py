6a0a0b05bf886115be45a5dfc567fe3069e578e4
R-Plascencia <runnerdude08@gmail.com>
2017-03-14 23:54:27 -0700

    Changes to utils.py for newspaper addition and keywords

--- newsitems/utils.py
+++ newsitems/utils.py
@@ -2,8 +2,11 @@ from interests.models import Interest
 from .models import NewsItem
 from .models import NewsResult
 from django.utils import timezone
+from django.utils.html import strip_tags
+from newspaper import Article
 import feedparser
 import re
+
 
 
 # Dictionary of all allowed RSS sources of news
@@ -17,6 +20,137 @@ rss_sources = {'AP': 'http://hosted2.ap.org/atom/APDEFAULT/89ae8247abe8493fae244
     'WSJ': 'http://www.wsj.com/xml/rss/3_7085.xml',
     'Amnesty International': 'http://www.amnestyusa.org/rss/news/Americas/rss.xml'
 }
+
+# English stopwords that we don't care about
+cachedStopWords = [
+'i',
+'me',
+'my',
+'myself',
+'we',
+'our',
+'ours',
+'ourselves',
+'you',
+'your',
+'yours',
+'yourself',
+'yourselves',
+'he',
+'him',
+'his',
+'himself',
+'she',
+'her',
+'hers',
+'herself',
+'it',
+'its',
+'itself',
+'they',
+'them',
+'their',
+'theirs',
+'themselves',
+'what',
+'which',
+'who',
+'whom',
+'this',
+'that',
+'these',
+'those',
+'am',
+'is',
+'are',
+'was',
+'were',
+'be',
+'been',
+'being',
+'have',
+'has',
+'had',
+'having',
+'do',
+'does',
+'did',
+'doing',
+'a',
+'an',
+'the',
+'and',
+'but',
+'if',
+'or',
+'because',
+'as',
+'until',
+'while',
+'of',
+'at',
+'by',
+'for',
+'with',
+'about',
+'against',
+'between',
+'into',
+'through',
+'during',
+'before',
+'after',
+'above',
+'below',
+'to',
+'from',
+'up',
+'down',
+'in',
+'out',
+'on',
+'off',
+'over',
+'under',
+'again',
+'further',
+'then',
+'once',
+'here',
+'there',
+'when',
+'where',
+'why',
+'how',
+'all',
+'any',
+'both',
+'each',
+'few',
+'more',
+'most',
+'other',
+'some',
+'such',
+'no',
+'nor',
+'not',
+'only',
+'own',
+'same',
+'so',
+'than',
+'too',
+'very',
+'can',
+'will',
+'just',
+'don',
+'should',
+'would',
+'now',
+'-',
+]
 
 '''
 Helper function that takes all the sources and RSS links in the rss_sources dictionary and
@@ -57,9 +191,24 @@ def build_results(request, interest):
         for story in entries:
             description = story.description.encode('utf-8')
             description = str(description)
-            description = description.split('<div', 1)[0]
+            description = strip_tags(description)
             title = story.title
             link = story.link
+
+            # Use newspaper to parse the article into useful pieces
+            article = Article(link)
+            article.download()
+            article.parse()
+
+            # Find the keywords in the article's text (or in the desc if somehow newspaper doesn't succeed
+            # in getting the text back)
+            if article.text == '' or article.text == None:
+                keywords_found = find_keywords(description)
+            else:
+                text = article.text.encode('utf-8')
+                keywords_found = find_keywords(str(text))
+            print(keywords_found)
+            return None
 
             # Search for the associated Interest keywords in the desctiptions and titles of every story
             matches = 0
@@ -92,3 +241,25 @@ def build_results(request, interest):
                 result_found = False
                 enough_matches = False
     return results
+
+'''
+Finds keywords for a given block of text, creates a dictionary with the words and their frequency,
+converts this into a list of tuples containing (count, word), then iterates through the top n
+items in the list and returns the word portion of the tuple
+
+Returns: keywords list with top n keywords (defaults at 5)
+'''
+def find_keywords(text, n=5):
+    text = ' '.join([word for word in text.split() if word not in cachedStopWords])
+    wordlist = text.split()
+    word_freq = [wordlist.count(p) for p in wordlist]
+    kw_dict = dict(zip(wordlist, word_freq))
+
+    aux = [(kw_dict[key], key) for key in kw_dict]
+    aux.sort()
+    aux.reverse()
+    topword_list = []
+    for i in range(n):
+        topword_list.append(aux[i][1])
+
+    return topword_list
