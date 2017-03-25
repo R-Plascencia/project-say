b42eff47bb56156f658ded003207154607174019
R-Plascencia <runnerdude08@gmail.com>
2017-03-25 02:17:54 -0700

    Added new API resources
    Added api.py which has api endpoints for User and UserProfile

---
+++ accounts/api.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from tastypie.resources import ModelResource
+from tastypie import fields
+from tastypie.authorization import Authorization
+from .models import UserProfile
+
+
+class UserProfileResource(ModelResource):
+    interests = fields.ToManyField('interests.api.InterestResource', 'interests')
+    class Meta:
+        queryset = UserProfile.objects.all()
+        resource_name = 'user_profile'
+        authorization = Authorization()
+
+class UserResource(ModelResource):
+    profile = fields.ForeignKey(UserProfileResource, 'profile', full=True)
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+        excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined', 'resource_uri']
+        authorization = Authorization()
